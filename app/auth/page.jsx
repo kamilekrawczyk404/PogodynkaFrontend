9aa11a3ba05ef208ff0beae9566d340a0e360f83
kamilekkrawczyk404 <kamilekkrawczyk404@gmail.com>
2025-03-31 23:45:34 +0200

    other files

---
+++ app/auth/page.jsx
@@ -0,0 +1,5 @@
+export default function Auth() {
+    return (
+        <div className={'text-white'}>Elo</div>
+    )
+}
---
+++ components/navigation.jsx
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+import Link from 'next/link'
+
+const Navigation = () => {
+    return (
+        <div><Link href={'/'}>Pogodynka</Link></div>
+    )
+}
+
+export default Navigation
